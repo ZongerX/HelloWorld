335feeb61a53226003090052c394095affffbb6c
Chiji <Chiji@ZONGERX-PC>
2024-05-26 21:34:28 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>		
+using namespace std;
+
+int main() {
+	int i = 0;
+	setlocale(LC_ALL, "Ru");
+	cout << "sanya huy sosi, okay?" << endl;
+	do {
+		cout << "Саня, хуй соси, ок? " << i << endl;
+		i++;
+	} while (i < 100);
+	cout << "						Поздравляем, вы хуесос!" << endl;
+	cin >> i;
+	return 0;
+}
+
+
